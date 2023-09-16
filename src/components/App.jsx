9c82625e7e7426a65e3dc06aede6c7198ea53a76
Artchem <chemerys.artem@gmail.com>
2023-09-16 23:48:26 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Statistics
+        good={0}
+        neutral={0}
+        bad={0}
+        total={0}
+        positivePercentage={0}
+      ></Statistics>
     </div>
   );
 };
