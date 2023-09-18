2ce0dd020481467d8cde14e3c5d5357c742978fa
Artchem <chemerys.artem@gmail.com>
2023-09-18 23:03:43 +0300

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,97 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Statis } from './Statistics.styled';
 
 export class Statistics extends React.Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positivePercentage: 0,
-  };
-
-  static propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    positivePercentage: PropTypes.number,
-  };
-
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
-    total: this.props.total,
-    positivePercentage: this.props.positivePercentage,
-  };
-
-  handleIncrementGood = () => {
-    console.log('Click good');
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-    this.countTotalFeedback();
-    // console.log(this);
-  };
-
-  handleIncrementNeutral = () => {
-    console.log('Click neutral');
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-    this.countTotalFeedback();
-    // console.log(this);
-  };
-
-  handleIncrementBad = () => {
-    console.log('Click bad');
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-    // console.log(this);
-  };
-
-  countTotalFeedback() {
-    // console.log(this.state.good);
-    const totalFeedback = this.state.bad + this.state.good + this.state.neutral;
-    this.setState({ total: totalFeedback });
-  }
-
-  countPositiveFeedbackPercentage() {
-    const countPositive = ((this.state.total + 1) / this.state.good) * 100;
-    console.log(this.state.total);
-    // this.setState({ positivePercentage: countPositive });
-  }
-
   render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
+    // console.log(this.props);
     return (
-      <div>
-        <h2>Please leave feedback</h2>
-        <button type="button" onClick={this.handleIncrementGood}>
-          Good
-        </button>
-        <button type="button" onClick={this.handleIncrementNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.handleIncrementBad}>
-          Bad
-        </button>
-        <p>Statistics</p>
-        <span>
-          Good: <span>{this.state.good}</span>
-        </span>
-        <span>
-          Neutral: <span>{this.state.neutral}</span>
-        </span>
-        <span>
-          Bad: <span>{this.state.bad}</span>
-        </span>
-        <span>
-          Total: <span>{this.state.total}</span>
-        </span>
-        <span>
-          Positive feedback: <span>{this.state.positivePercentage}</span>
-        </span>
-      </div>
+      <Statis>
+        <span>Good: {good}</span>
+        <span>Neutral: {neutral}</span>
+        <span>Bad: {bad}</span>
+        <span>Total: {total}</span>
+        <span>Positive feedback: {positivePercentage}%</span>
+      </Statis>
     );
   }
 }
-
-// export const Statistics = () => {};
